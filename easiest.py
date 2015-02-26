a55fc1be1294b22d8279dfb3bdee5010a7a51220
Christopher Stewart <stewpot64@gmail.com>
2015-02-26 13:23:17 +0000

    Added solution to easiest.

---
+++ easiest.py
@@ -0,0 +1,26 @@
+"""
+http://open.kattis.com/easiest
+"""
+
+import sys
+
+
+def sum_digits(number):
+    sum_of_digits = 0
+    while number:
+        sum_of_digits, number = sum_of_digits + number % 10, number // 10
+    return sum_of_digits
+
+
+for line in sys.stdin:
+    n = int(line)
+
+    if n == 0:
+        break
+
+    p = 11
+    while True:
+        if sum_digits(n) == sum_digits(n * p):
+            print(p)
+            break
+        p += 1
